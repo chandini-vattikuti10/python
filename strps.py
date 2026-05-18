b31795abeda21ec1b3122bad84e7744f368bcb61
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-05-18 11:26:51 +0530

    functions using string methods

---
+++ strps.py
@@ -0,0 +1,33 @@
+# Write a Python function to convert a given string
+# to uppercase using the upper() method.
+
+# def str_to_upper(str):
+#     return str.upper()
+
+
+# text = "python programming language"
+# print(str_to_upper(text))
+
+
+# Use the replace() method to write a function
+# that replaces all occurrences of a specified substring
+# with another substring in a given string.
+
+# def replace_substring(str):
+#     return str.replace('i', 'a')
+
+
+# text = "Hii. i am chandini"
+# print(replace_substring(text))
+
+
+# Create a function that checks if a given string
+# starts with a specific substring using the startswith() method,
+#  returning True if it does and False otherwise.
+
+def starts_with(str):
+    return str.startswith('hii')
+
+
+text = "hii my name is chandini"
+print(starts_with(text))
