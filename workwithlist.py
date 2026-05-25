c56c0c8a5507242e625ad31cb47e21bd90909480
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-05-25 22:01:01 +0530

    working with lists

---
+++ workwithlist.py
@@ -0,0 +1,9 @@
+# Create a list containing the names of 5 different fruits and assign it to a variable.
+
+l=['apple','banana','grapes','orange','kiwi']
+
+# Use indexing to access and print the third item in the list.
+print(l[3])
+# Append a new fruit to the end of the list and then print the entire updated list.
+l.append('mango')
+print(l)
