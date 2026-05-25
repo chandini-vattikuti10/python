d2b3a18e2e90800e3e00dee48ee4673a218f985f
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-05-25 21:55:20 +0530

    list methods

---
+++ listmethods.py
@@ -0,0 +1,12 @@
+# Create a list of numbers (1 through 10) and use the append method to add the number 11 to the end of the list.
+l=[1,2,3,4,5,6,7,8,9,10]
+l.append(11)
+print(l)
+
+# Use the insert method to place the number 0 at the beginning of the list.
+l.insert(0,0)
+print(l)
+
+# Use the remove method to delete the number 5 from the list, then print the updated list to verify the changes.
+l.remove(5)
+print(l)
