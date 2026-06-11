a07afe6144200148dc2e68a2791ea8ba1858886f
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-06-11 10:11:24 +0530

    Functions introduction

---
+++ functionsintro.py
@@ -0,0 +1,17 @@
+'''1. Define a function greet that takes a name as an argument and prints out a personalized greeting message.'''
+# def greet(a):
+#     print("Good Morning",a)
+# greet("Chandini")
+'''2. Create a function add_numbers that accepts two numbers as arguments and returns their sum. Test this function with different input values.'''
+# def add_numbers(a,b):
+#     return a+b
+# print(add_numbers(2,3))
+# print(add_numbers(10,20))
+'''3. Write a function is_even that checks if a given number is even or odd, returning True for even numbers 
+and False for odd numbers. Use this function to classify a list of numbers as even or odd.'''
+
+# def is_even(num):
+#     return num % 2 == 0
+# numbers = [10, 15, 22, 33, 40, 51]
+# classification = {num: "True" if is_even(num) else "False" for num in numbers}
+# print(classification)
