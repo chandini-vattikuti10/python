5f1fb5c2f3e1ff47ff8383417a822a068665d774
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-06-12 10:51:19 +0530

    Applying Functions in Programming

---
+++ functask.py
@@ -0,0 +1,18 @@
+'''Create a function named add_numbers that takes two parameters and returns their sum.'''
+
+# def add_numbers(a,b):
+#     return a+b
+# print(add_numbers(45,55))
+
+'''Develop a function named calculate_area that takes the radius of a circle as input 
+and returns the area of the circle, using the formula πr².'''
+
+# def calculate_area(r):
+#     return 3.14*r*r
+# print(calculate_area(5))
+
+'''Write a function named greet_user that accepts a user's name as input 
+and prints out a personalized greeting message.'''
+# def greet_user(name):
+#     print("Hii",name,'Good Morning.')
+# greet_user('chandini')
