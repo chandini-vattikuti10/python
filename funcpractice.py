61070b8d5e655ec5c29047f8d819d96b51f24236
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-06-12 11:00:25 +0530

    practicing functions with parameters

---
+++ funcpractice.py
@@ -0,0 +1,20 @@
+'''Create a function named greet that takes a person's name as a 
+parameter and prints out a personalized greeting message.'''
+
+# def greet(name):
+#     print("welcome",name)
+# greet("Chandini")
+
+'''Write a function called sumNumbers that accepts two numbers as parameters 
+and returns their sum.'''
+
+# def sumNumbers(a,b):
+#     return a+b
+# print(sumNumbers(12,13))
+
+'''Develop a function named convertTemperature that takes a temperature in Celsius 
+as a parameter and returns the equivalent temperature in Fahrenheit.'''
+
+def convertTemperature(a):
+    return 9/5*a+32
+print(convertTemperature(25))
