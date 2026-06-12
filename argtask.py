9eac5f3001db4cee39130205a42129782c661ddb
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-06-12 11:14:36 +0530

    working with arguments in python

---
+++ argtask.py
@@ -0,0 +1,19 @@
+'''Define a Python function that takes two arguments, adds them together, and returns the result.'''
+
+# def add_numbers(a,b):
+#     return a+b
+# print(add_numbers(10,20))
+# print(add_numbers(a=20,b=30))
+
+'''Write a function that accepts a variable number of arguments and prints each one to the console.'''
+# def print_arguments(*args):
+#     for arg in args:
+#         print(arg)
+# print_arguments("Python", 10, 3.14, True)
+'''Create a function that accepts a single argument, a list of numbers, and returns the average value of the numbers in the list.'''
+# def average(numbers):
+#     if len(numbers) == 0:
+#         return 0
+#     return sum(numbers) / len(numbers)
+# nums = [10, 20, 30, 40, 50]
+# print("Average:", average(nums))
