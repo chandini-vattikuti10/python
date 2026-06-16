c6cc780d3208d53b9bb5ffb661d091591fbac855
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-06-16 10:37:46 +0530

    random module task

---
+++ rantask.py
@@ -0,0 +1,29 @@
+'''1. Generate a random integer between 1 and 100 (inclusive) and print it out. 
+Use this number to simulate a coin toss where even numbers represent heads and odd numbers represent tails.'''
+
+# import random
+# n = random.randint(1, 100)
+# print("Random integer:", n)
+# if n % 2 == 0:
+#     print("Coin toss result: Heads")
+# else:
+#     print("Coin toss result: Tails")
+
+'''2. Create a list of 10 random floating-point numbers between 0 and 1, then calculate and print the average of these numbers.'''
+# import random
+# sum=0
+# for x in range(10):
+#     r=random.random()
+#     sum+=r
+# print(sum/10)
+
+'''3. Simulate a dice roll by generating a random integer between 1 and 6, and 
+print out the result along with a message indicating whether the roll was a "success" (if the roll is 6) or not.'''
+
+# import random
+# r=random.randint(1,6)
+# print(r)
+# if r==6:
+#     print("success")
+# else:
+#     print('try again')
