39a5a61c6424e0af91f34459ec6e6882b65c62dc
Chandini Vattikuti <chandinivattikuti@Chandinis-MacBook-Air.local>
2026-06-25 10:13:54 +0530

    operatitons using lambda function

---
+++ lambdatask.py
@@ -0,0 +1,28 @@
+'''1. Implement a lambda function to sort a list of dictionaries based on a specific key.'''
+
+# students = [
+#     {"name": "Alice", "marks": 85},
+#     {"name": "Bob", "marks": 72},
+#     {"name": "Charlie", "marks": 90}
+# ]
+
+# sorted_students = sorted(students, key=lambda x: x["marks"])
+
+# print("Sorted List:")
+# for student in sorted_students:
+#     print(student)
+
+
+'''2. Use a lambda function as a filtering criterion to extract odd numbers from a given list of integers.'''
+
+# numbers = [10, 15, 20, 25, 30, 35, 40]
+# odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+
+# print("Odd Numbers:", odd_numbers)
+
+'''3. Create a lambda function to calculate the square of each number in a list and return the results as a new list.'''
+
+# numbers = [1, 2, 3, 4, 5]
+# squares = list(map(lambda x: x ** 2, numbers))
+
+# print("Squares:", squares)
