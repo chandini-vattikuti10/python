a28d89c06229dd8a263e114675c4e3bb6b10c8ba
chandini-vattikuti10 <vattikutichandini@gmail.com>
2026-07-01 09:51:46 +0530

    constructor and instance variables

---
+++ coninstance.py
@@ -0,0 +1,46 @@
+'''Create a Person class with a constructor that initializes name, age, and city instance variables.'''
+
+# class Person:
+#     def __init__(self, name, age, city):
+#         self.name = name
+#         self.age = age
+#         self.city = city
+# p1 = Person("Chandini", 22, "Hyderabad")
+# print(p1.__dict__)
+
+'''Implement a method to display the person's information, using the instance variables to construct the output.'''
+
+# class Person:
+#     def __init__(self):
+#         self.name = 'chandini'
+#         self.age = 21
+#         self.city = 'hyderabad'
+
+#     def display(self):
+#         print("Name :", self.name)
+#         print("Age  :", self.age)
+#         print("City :", self.city)
+# p1 = Person()
+# p1.display()
+
+
+'''Develop a main method to create at least two Person objects with different attributes and 
+demonstrate the usage of the display method for each object.'''
+
+# class Person:
+#     def __init__(self, name, age, city):
+#         self.name = name
+#         self.age = age
+#         self.city = city
+
+#     def display(self):
+#         print("Name :", self.name)
+#         print("Age  :", self.age)
+#         print("City :", self.city)
+#         print()
+
+# person1 = Person("Chandini", 22, "Hyderabad")
+# person2 = Person("Rahul", 25, "Vijayawada")
+
+# person1.display()
+# person2.display()
